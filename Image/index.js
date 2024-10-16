d210fef12e6e99eb6b8ca0f156fd1409da52472a
AnnemPoojitha <annempoojitha1@gmail.com>
2024-10-16 12:29:09 +0530

    Add files via upload

---
+++ Image/index.js
@@ -0,0 +1,11 @@
+function ChangeImgAngular(){
+    var imageref=document.getElementById('image')
+    imageref.src="https://th.bing.com/th/id/OIP.QvjTngLB7Rtg45bTl3W6vwHaHa?rs=1&pid=ImgDetMain"
+    imageref.style.borderColor="lightsalmon"
+}
+function ChangeImgReact(){
+    var imageref=document.getElementById('image')
+    imageref.src="https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png"
+    imageref.style.borderColor="lightskyblue"
+
+}
